3865309b5902883b76ef52311199b9fa75b76bd8
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-09-18 17:41:04 +0300

    upd

--- lab-runner/src/lab3/ExpressionCounter.java
+++ lab-runner/src/lab3/ExpressionCounter.java
@@ -3,68 +3,27 @@ package lab3;
 import tasks.Task;
 import utils.ConsoleUtils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class ExpressionCounter extends Task {
-    private double x = 0;
-    private static final String xString = "X";
-    private static final String operations = "+-";
-    private static final String operationsRegExpr = "[-+]";
+    private static final String xString = "x";
 
     @Override
     public void run() {
-        String _expression = ConsoleUtils.inputString("Input expression: ");
-        if (_expression.contains(xString)) {
-            x = ConsoleUtils.inputNumber("Input x: ");
+        String expression = ConsoleUtils.inputString("Input expression: ");
+        if (expression.contains(xString)) {
+            int x = (int)ConsoleUtils.inputNumber("Input x: ");
+            expression = expression.replaceAll(xString, Integer.toString(x));
         }
-        try {
-            double result = evaluate(_expression);
-            System.out.println("Result is " + result + ".");
-        } catch (Exception e) {
-            System.out.println("An error occurred! \n" + e);
-        } finally {
-            tryAgain();
-        }
+        System.out.println("Result is " + evaluate(expression));
     }
 
-    private double evaluate(String _expression) {
-        if (_expression.startsWith("-") || _expression.startsWith("+")) {
-            _expression = "0" + _expression;
-        }
-        var tokens = Arrays.stream(_expression.split(operationsRegExpr)).toList();
-        var operationsList = getOperands(_expression);
-        return tryEvaluate(tokens, operationsList);
-    }
-
-    private double tryEvaluate(List<String> tokens, List<String> operations) {
-        if (tokens.size() == 0) {
-            throw new IllegalArgumentException("Tokens are empty!");
-        }
-        double result = tokens.get(0).equals(xString) ? x : Double.parseDouble(tokens.get(0));
-        for (int i = 1; i < tokens.size(); ++i) {
-            var t = tokens.get(i);
-            double value = t.equals(xString) ? x : Double.parseDouble(tokens.get(i));
-            if (operations.get(i - 1).equals("+")) {
-                result += value;
-            } else if (operations.get(i - 1).equals("-")) {
-                result -= value;
-            } else {
-                throw new IllegalArgumentException("String contains wrong operand " + operations.get(i - 1));
-            }
-        }
-        return result;
-    }
-
-    private List<String> getOperands(String expr) {
-        var result = new ArrayList<String>();
-        for (int i = 0; i < expr.length(); ++i) {
-            var s = Character.toString(expr.charAt(i));
-            if (operations.contains(s)) {
-                result.add(s);
-            }
-        }
-        return result;
+    private int evaluate(String expression) {
+        expression = expression
+                .replace(" ", "")
+                .replace("-", " -")
+                .replace("+", " +");
+        int[] arr = Arrays.stream(expression.split(" ")).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(arr).sum();
     }
 }
--- lab-runner/src/lab3/SuffixReplacer.java
+++ lab-runner/src/lab3/SuffixReplacer.java
@@ -11,19 +11,15 @@ public class SuffixReplacer extends Task {
     @Override
     public void run() {
         var expression = ConsoleUtils.inputString("Input text: ");
-        var words = expression.split(spaceRegex);
-        replaceEndings(words);
-        System.out.println("Result is: " + String.join(" ", words));
+        System.out.println("Result is: " + replaceEndings(expression));
         tryAgain();
     }
 
-    void replaceEndings(String[] words) {
-        for (int i = 0; i < words.length; ++i) {
-            var word = words[i];
-            if (word.endsWith("ing")) {
-                int length = word.length();
-                words[i] = word.substring(0, length - suffixToReplace.length()) + newSuffix;
-            }
+    String replaceEndings(String expression) {
+        var newStr = expression.replace(suffixToReplace + " ", newSuffix + " ");
+        if (newStr.endsWith(suffixToReplace)) {
+            newStr = newStr.substring(0, newStr.length() - suffixToReplace.length()) + newSuffix;
         }
+        return newStr;
     }
 }
